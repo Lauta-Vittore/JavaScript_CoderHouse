e92fd6a2263dcd99154830a9572cbc25a4b6d5ca
Lauta-Vittore <lautarotin@gmail.com>
2022-10-04 17:06:16 -0300

    ok

--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
-let precio = parseInt(prompt('Ingresa el precio del producto para calcular las cuotas'))
+let stock = [
+    {nombre: 'remera', precio: 1500, talla: 'xl'},
+    {nombre: 'buzo', precio: 4500, talla: 's'},
+    {nombre: 'zapatillas', precio: 11000, talla: '42'},
+    {nombre: 'cuellito', precio: 1000},
+] 
 
-while(isNaN(precio) || precio <=0 ){
-        precio =parseInt(prompt('Dato incorrecto! \nIngresa el precio del producto para calcular las cuotas'))
-}
+let producto = prompt('ingresar producto a comprar:\n 1 - remera \n 2 - buzo \n 3 - zapatillas \n 4 - cuellito' )
+
+const resultado = stock.find( productos => productos.nombre === producto)
+console.log(resultado.precio);
+
 
 let cuotas = parseInt(prompt('Ingresa cantidad de cuotas:\n 1 cuota: sin interés\n 3 cuotas: 5% de interes\n 6 cuotas: 10% de interés\n 12 cuotas: 15% de interés'))
-while( !(cuotas == 1 || cuotas == 3 || cuotas == 6 || cuotas == 12) || isNaN(precio) ){
+while( !(cuotas == 1 || cuotas == 3 || cuotas == 6 || cuotas == 12) || isNaN(resultado.precio) ){
     cuotas = parseInt(prompt('Dato incorrecto!\nIngresa cantidad de cuotas:\n 1 cuota: sin interés\n 3 cuotas: 5% de interes\n 6 cuotas: 10% de interés\n 12 cuotas: 15% de interés'))
 }
 
-let precioConInteres = precioInteres( precio, cuotas)
+let precioConInteres = precioInteres( resultado.precio, cuotas)
 
 alert('el precio con interes incluido es: $' + precioConInteres.toFixed(2) + '\n La cuota es de: $' + (precioConInteres/cuotas).toFixed(2))
 
